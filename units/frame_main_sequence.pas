e9069e4892862a1f16b0ba54ff83489a7084c96b
Lulu <cacameur@gmx.fr>
2024-07-06 07:31:04 +0200

    Added help button and Add sequence button

--- units/frame_main_sequence.pas
+++ units/frame_main_sequence.pas
@@ -17,13 +17,17 @@ type
   { TFrameMainSequence }
 
   TFrameMainSequence = class(TFrame)
+    BHelp: TSpeedButton;
     Label9: TLabel;
     Panel1: TPanel;
     Panel2: TPanel;
     Panel3: TPanel;
     Panel4: TPanel;
+    BAddSequence: TSpeedButton;
     Splitter1: TSplitter;
     Splitter2: TSplitter;
+    procedure BAddSequenceClick(Sender: TObject);
+    procedure BHelpClick(Sender: TObject);
     procedure Splitter1Moved(Sender: TObject);
   private
     FRedocking: boolean;
@@ -50,10 +54,13 @@ type
     procedure LoadProjectOptionsFrom(t: TStringList);
 
     procedure UpdateLayout;
+
+    procedure UpdateEditMode;
   end;
 
 implementation
-uses u_project_manager, u_list_sequence, u_utils, u_logfile, PropertyUtils;
+uses u_project_manager, u_list_sequence, u_utils, u_logfile, form_help,
+  u_resource_string, PropertyUtils, Math;
 
 {$R *.lfm}
 
@@ -63,20 +70,29 @@ procedure TFrameMainSequence.Splitter1Moved(Sender: TObject);
 begin
   if FRedocking then exit;
 
-  if Sender = Splitter1 then
+{  if Sender = Splitter1 then
     FWantedTopSplitter1 := Splitter1.Top-1;
   if Sender = Splitter2 then
-    FWantedTopSplitter2 := Splitter2.Top-1;
+    FWantedTopSplitter2 := Splitter2.Top-1;   }
 
   Project.Options.Save;
   //RedockPanels;
 end;
 
+procedure TFrameMainSequence.BHelpClick(Sender: TObject);
+begin
+  _ShowHelp(HelpSequenceList, BHelp);
+end;
+
+procedure TFrameMainSequence.BAddSequenceClick(Sender: TObject);
+begin
+  FrameViewTopList1.MINewSequenceClick(NIL);
+end;
+
 procedure TFrameMainSequence.RedockPanels;
 begin
   FRedocking := True;
-  Splitter1.Top := FWantedTopSplitter1;
-  Splitter2.Top := FWantedTopSplitter2;
+
   FRedocking := False;
 end;
 
@@ -98,8 +114,8 @@ begin
   FrameIntermissionMusic1.Align := alClient;
 
 
-  FWantedTopSplitter1 := Panel1.Height;
-  FWantedTopSplitter2 := Panel3.Height;
+//  FWantedTopSplitter1 := Panel1.Height;
+//  FWantedTopSplitter2 := ClientHeight - Panel3.Height;
 end;
 
 procedure TFrameMainSequence.EraseBackground(DC: HDC);
@@ -139,14 +155,14 @@ begin
   Project.SetModified;
 end;
 
-const PROJECTOPTION_FRAMESEQUENCER_HEADER='[FRAME_SEQUENCER]';
+const PROJECTOPTION_FRAMESEQUENCE_HEADER='[MAIN_VIEW_SEQUENCE_LIST]';
 procedure TFrameMainSequence.SaveProjectOptionsTo(t: TStringList);
 var prop: TProperties;
 begin
   prop.Init('|');
-  prop.Add('HeightPanelProjectList', FWantedTopSplitter2); // Panel3.Height);
-  prop.Add('HeightPanelSequenceList', FWantedTopSplitter1); // Panel1.Height);
-  t.Add(PROJECTOPTION_FRAMESEQUENCER_HEADER);
+  prop.Add('SplitterIntermission', Splitter1.Top);
+  prop.Add('SplitterProjectList', Splitter2.Top);
+  t.Add(PROJECTOPTION_FRAMESEQUENCE_HEADER);
   t.Add(prop.PackedProperty);
 end;
 
@@ -154,23 +170,29 @@ procedure TFrameMainSequence.LoadProjectOptionsFrom(t: TStringList);
 var prop: TProperties;
   k: Integer;
 begin
-  k := t.IndexOf(PROJECTOPTION_FRAMESEQUENCER_HEADER);
+  k := t.IndexOf(PROJECTOPTION_FRAMESEQUENCE_HEADER);
   if (k = -1) or (k = t.Count-1) then
     prop.SetEmpty
   else
     prop.Split(t.Strings[k+1], '|');
 
-  prop.IntegerValueOf('HeightPanelSequenceList', k, Height*3 div 5);
-  Panel1.Height := k;
-  FWantedTopSplitter1 := k;
-  prop.IntegerValueOf('HeightPanelProjectList', k, Height div 5);
-  Panel3.Height := k;
-  FWantedTopSplitter2 := k;
+  prop.IntegerValueOf('SplitterProjectList', k, Height*4 div 5);
+  k := EnsureRange(k, ClientHeight*2 div 3, ClientHeight);
+  Splitter2.Top := k;
+
+  prop.IntegerValueOf('SplitterIntermission', k, Height*3 div 5);
+  k := EnsureRange(k, Height*3 div 5, Splitter2.Top-Splitter1.Height);
+  Splitter1.Top := k;
 end;
 
 procedure TFrameMainSequence.UpdateLayout;
 begin
   RedockPanels;
+end;
+
+procedure TFrameMainSequence.UpdateEditMode;
+begin
+  BAddSequence.Visible := Project.Options.EditMode;
 end;
 
 end.
